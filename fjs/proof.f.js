89e1128e1008ad2666899e106460c5e872949683
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-08-03 10:20:30 -0700

    update

--- fjs/proof.f.js
+++ fjs/proof.f.js
@@ -1,1 +1,5 @@
-export const proof = () => { throw "not implemented" }
+import { assert } from 'functionalscript/fjs/asserts/module.f.js'
+
+export const proof = () => {
+    assert(2 + 2 === 4, '2 + 2 should equal 4')
+}
--- proof.f.js
+++
@@ -1,5 +0,0 @@
-import { assert } from 'functionalscript/fjs/asserts/module.f.js'
-
-export const proof = () => {
-    assert(2 + 2 === 4, '2 + 2 should equal 4')
-}
